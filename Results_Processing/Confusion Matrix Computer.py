3eec35e5ca2ad51919e6f8ba3e47ba85787d34bf
atdunwoody <146408382+atdunwoody@users.noreply.github.com>
2023-11-30 12:38:32 -0700

    Conf_matrix cleaned up excess fx

--- Results_Processing/Confusion Matrix Computer.py
+++ Results_Processing/Confusion Matrix Computer.py
@@ -14,76 +14,17 @@ import numpy as np # math and array handling
 import matplotlib.pyplot as plt # plot figures
 
 import pandas as pd # handling large data as table sheets
-from sklearn.metrics import classification_report, accuracy_score,confusion_matrix  # calculating measures for accuracy assessment
-
-
-import seaborn as sn
-
-
-
-
-
-def read_raster_normal_multiband(raster_path):
-    ds = gdal.Open(raster_path, gdal.GA_ReadOnly)
-    raster_bands = []
-    
-    for b in range(ds.RasterCount):
-        band = ds.GetRasterBand(b + 1)
-        raster_array = band.ReadAsArray()
-        raster_bands.append(raster_array)
-    
-    return raster_bands
-
-
-def compute_confusion_matrix(target_raster_path, reference_raster_path, results_path, band_index=0):
-    print('Opening raster: ' + target_raster_path)
-    target_raster_bands = read_raster_normal_multiband(target_raster_path)
-    target_raster = target_raster_bands[band_index]  # Select the specific band
-
-    print('Opening raster: ' + reference_raster_path)
-    reference_raster_bands = read_raster_normal_multiband(reference_raster_path)
-    reference_raster = reference_raster_bands[band_index]  # Select the specific band
-
-    # Apply masking to the target raster
-    mask = np.copy(target_raster)
-    mask[mask > 0.0] = 1.0  # Non-zero pixels are marked with 1.0
-
-    # Flatten the raster arrays
-    target_flat = (target_raster * mask).flatten()
-    reference_flat = reference_raster.flatten()
-
-    # Define your class labels as strings
-    labels = [1, 2]
-
-    # Calculate confusion matrix and statistics
-    print('Calculating confusion matrix...')
-    cm = confusion_matrix(reference_flat, target_flat, labels=labels)
-    with open(results_path, 'w') as file:
-        file.write('Confusion Matrix:\n')
-        file.write(pd.DataFrame(cm, index=['True:' + lab for lab in labels],
-                                columns=['Predicted:' + lab for lab in labels]).to_string() + '\n\n')
-    
-    print('Calculating precision and recall...')
-    precision = precision_score(reference_flat, target_flat, labels=labels, average=None)
-    recall = recall_score(reference_flat, target_flat, labels=labels, average=None)
-
-    # Save results to a file
-    print('Saving results to: ' + results_path)
-    with open(results_path, 'w') as file:
-        file.write('Precision for each class:\n')
-        for label, p in zip(labels, precision):
-            file.write(f'Class {label}: {p:.3f}\n')
-        file.write('\nRecall for each class:\n')
-        for label, r in zip(labels, recall):
-            file.write(f'Class {label}: {r:.3f}\n')
-
-import os
-import numpy as np
-import pandas as pd
-from osgeo import gdal, gdal_array, ogr
 from sklearn.metrics import classification_report, accuracy_score
 
+
+
 def compute_confusion_matrix_v2(target_raster, img_RS, results_txt):
+    #Print input parameters to txt file
+    print('Reference raster: {}'.format(img_RS), file=open(results_txt, "a"))
+    print('Target raster: {}'.format(target_raster), file=open(results_txt, "a"))
+    #Print a new line that labels the start of the confusion matrix
+    print('----------Confusion Matrix:----------------', file=open(results_txt, "a"))
+    
     # Open the reference raster for reading
     ref_ds = gdal.Open(img_RS, gdal.GA_ReadOnly)
     ref_img = ref_ds.ReadAsArray()
